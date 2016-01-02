e88ce353cbba60677ad3e967ef0d3ddeeeff74d5
Graham Clapham <info@gcdesign.co.uk>
2016-01-02 18:47:39 +0000

    Amended test.

--- test/spirograph-test.js
+++ test/spirograph-test.js
@@ -5,17 +5,32 @@ describe("A test suite", function(){
 });
 
 var _spiro;
-var _element = document.createElement('div')
-_spiro = new SpiroGraph(_element);
 
 describe("The Spirograph should be a ble to be instatiated passing a DOM element as an argument.", function(){
+
+    var _element = document.createElement('div')
+    _spiro = new SpiroGraph(_element);
 
     it("Should be instantiated", function(){
         expect(_spiro).not.toBe(null);
     })
 });
 
+describe("The Spirograph should be a ble to be instatiated passing a String as an argument for the DOM element's ID.", function(){
 
+
+        var _element2 = document.createElement('div');
+        _element2.setAttribute("ID","HolderDiv");
+        var _body = document.querySelector('body');
+        console.log("_element2 ",_element2);
+       // _spiro = new SpiroGraph(HolderDiv);
+
+
+
+    it("Should be instantiated with a String for the DOM element ID.", function(){
+        expect(_spiro).not.toBe(null);
+    })
+});
 
 
 
